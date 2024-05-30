e803821f52a689dfece350b372a2b4784f365580
PhucQuach1907 <phucquach1907@gmail.com>
2024-05-31 01:12:23 +0700

    feat: create backend for doctor service

--- doctor/doctor_info/models.py
+++ doctor/doctor_info/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+class Doctor(models.Model):
+    user_id = models.PositiveIntegerField()
+    specialty = models.CharField(max_length=255)
+    degree = models.CharField(max_length=255)
+
+    class Meta:
+        db_table = 'doctor'
