a5694422af2abe31602d27e8861517305174058b
PhucQuach1907 <phucquach1907@gmail.com>
2024-05-31 00:55:15 +0700

    feat: create backend for patient servce

--- patient/patient/settings.py
+++ patient/patient/settings.py
@@ -27,6 +27,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
+
 
 # Application definition
 
@@ -37,9 +40,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'patient_info',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -75,8 +81,12 @@ WSGI_APPLICATION = 'patient.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'hms-patient',
+        'USER': 'postgres',
+        'PASSWORD': 'phucquach2002',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
--- patient/patient_info/models.py
+++ patient/patient_info/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+class Patient(models.Model):
+    user_id = models.PositiveIntegerField()
+
+    class Meta:
+        db_table = 'patient'
